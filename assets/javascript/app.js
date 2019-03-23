965d84e36a3af79276120c13932180bd75b6c68a
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-03-23 10:12:16 -0700

    Initialize index.html and app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// Code is executed in strict mode
+"use strict";
+
+// Wrap all code in an anonymous function so functions cannot be called externally
+(function() {
+    $(document).ready(function() {
+
+    });
+})();
