efd1c1fb45670a3f329ee02e545ac2fe040f54aa
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-03-24 18:41:06 -0700

    Set initial layout and button functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,7 +3,42 @@
 
 // Wrap all code in an anonymous function so functions cannot be called externally
 (function() {
+
+    // Run JS once the document is ready
     $(document).ready(function() {
 
+        // Start the game
+        function start() {
+            hide($("#start"));
+            unhide($("#time"));
+            unhide($("#question"));
+            unhide($(".option"));
+            unhide($("#reset"));
+        }
+
+        // Reset the game
+        function reset() {
+            unhide($("#start"));
+            hide($("#time"));
+            hide($("#question"));
+            hide($(".option"));
+            hide($("#reset"));
+        }
+
+        // Hide jQuery element
+        function hide(el) {
+            el.addClass("hidden");
+        }
+
+        // Unhide jQuery element
+        function unhide(el) {
+            el.removeClass("hidden");
+        }
+
+        // Start the game when the start button is clicked
+        $("#start").on("click", start);
+
+        // Reset the game when the reset button is clicked
+        $("#reset").on("click", reset);
     });
 })();
