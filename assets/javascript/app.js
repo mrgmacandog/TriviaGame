88051eb60295abc36a0e1d22593ec95482bed086
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-03-24 21:57:26 -0700

    Add countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,13 +7,47 @@
     // Run JS once the document is ready
     $(document).ready(function() {
 
+        const startTime = 15;
+
+        // Time left
+        let timeLeft = startTime;
+
+        let intervalId;
+
         // Start the game
         function start() {
+            // Update what the display
             hide($("#start"));
             unhide($("#time"));
             unhide($("#question"));
             unhide($(".option"));
             unhide($("#reset"));
+
+            // Set time interval to countd down every 1 second
+            intervalId = setInterval(countDown, 1000);
+            
+            // Reset after 15 seconds
+            //setTimeout(reset, 15000);
+        }
+
+        function countDown() {
+            if (timeLeft > 0) {
+                // Decrease one second
+                timeLeft -= 1;
+
+                // Update the time remaining on the web page
+                if (timeLeft === 1) {  // Change "Seconds" to "Second" for 1 secsond remaining
+                    $("#seconds").text(timeLeft + " Second");
+                } else {
+                    $("#seconds").text(timeLeft + " Seconds");
+                }
+
+                console.log(timeLeft);
+            } else {
+                // Clear the interval once the time remaining is 0
+                clearInterval(intervalId);
+                alert("time's up");
+            }
         }
 
         // Reset the game
@@ -23,14 +57,17 @@
             hide($("#question"));
             hide($(".option"));
             hide($("#reset"));
+
+            // Reset the time to 15 seconds
+            timeLeft = startTime;
         }
 
-        // Hide jQuery element
+        // Hide jQuery element(s)
         function hide(el) {
             el.addClass("hidden");
         }
 
-        // Unhide jQuery element
+        // Unhide jQuery element(s)
         function unhide(el) {
             el.removeClass("hidden");
         }
