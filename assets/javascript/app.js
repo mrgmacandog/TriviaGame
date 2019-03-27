f83fb04e9332dd0d16ebc530c6653ff68ffcc14f
mrgmacandog <24902881+mrgmacandog@users.noreply.github.com>
2019-03-26 23:30:50 -0700

    Add ability to make a guess and continue to next question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,10 +13,13 @@
         // Initialize array of TriviaQuestion objects
         let questions;
 
+        let currentQuestion;
+
         // TriviaQuestion constructor
         //     Takes in a question and 4 options
-        function TriviaQuestion(question, options) {
+        function TriviaQuestion(question, answer, options) {
             this.question = question;
+            this.answer = answer;
             this.options = options;
         }
 
@@ -29,51 +32,61 @@
         function createQuestions() {
             questions = [
                 new TriviaQuestion("What is the name of bar the gang frequently go to?",
+                                   "MacLaren's",
                                    ["MacLaren's",
                                     "MacLoughlin's",
                                     "MacLure's",
                                     "MacMaster's"]),
                 new TriviaQuestion("What color and instrument does Ted steal for Robin?",
+                                   "Blue French Horn",
                                    ["Blue French Horn",
                                     "Blue Trumpet",
                                     "Red French Horn",
                                     "Red Trumpet"]),
                 new TriviaQuestion("Which acronym does Barney use to describe his job?",
+                                   "P.L.E.A.S.E.",
                                    ["P.L.E.A.S.E.",
                                     "S.E.C.R.E.T.",
                                     "W.O.R.K.",
                                     "S.T.O.P."]),
                 new TriviaQuestion("What is the first Robin Sparkles song the gang listens to?",
+                                   "Let's Go To The Mall",
                                    ["Let's Go To The Mall",
                                     "Sandcastles in the Sand",
                                     "The Beaver Song",
                                     "P.S. I Love You"]),
                 new TriviaQuestion("What are the names of Ted's children?",
+                                   "Luke & Penny",
                                    ["Luke & Penny",
                                     "Adam & Sara",
                                     "David & Jenna",
                                     "Jerry & Emily"]),
                 new TriviaQuestion("What do Barney and Ted want to name their bar if they had one?",
+                                   "Puzzles",
                                    ["Puzzles",
                                     "Clues",
                                     "Questions",
                                     "Kisses"]),
                 new TriviaQuestion("What year is it when Ted is telling his children how he met their mother?",
+                                   "2030",
                                    ["2030",
                                     "2015",
                                     "2020",
                                     "2025"]),
                 new TriviaQuestion("At what occasion does Ted find a goat in his apartment?",
+                                   "His 31th birthday",
                                    ["His 31th birthday",
                                     "His 30st birthday",
                                     "New Year's Day",
                                     "Halloween"]),
                 new TriviaQuestion("Who narrates the show?",
+                                   "Bob Saget",
                                    ["Bob Saget",
                                     "Jason Segel",
                                     "Josh Radnor",
                                     "Neil Patrick Harris"]),
                 new TriviaQuestion("How many seasons were there?",
+                                   "9",
                                    ["9",
                                     "7",
                                     "8",
@@ -86,8 +99,7 @@
             // Get random question
             getNewQuestion();
 
-            // Reset time
-            timeLeft = START_TIME;
+            
 
             // Update what the display shows
             hide($("#start"));
@@ -96,8 +108,7 @@
             unhide($(".option"));
             unhide($("#reset"));
 
-            // Set time interval to countd down every 1 second
-            intervalId = setInterval(countDown, 1000);
+            
             
             // Get new question after 15 seconds
             // setTimeout(getNewQuestion, 15000);
@@ -117,27 +128,46 @@
 
                 console.log(timeLeft);
             } else {
-                // Clear the interval once the time remaining is 0
-                clearInterval(intervalId);
+                console.log("ran out of time");
 
                 // Get random question
                 start();
             }
         }
 
+        function checkAnswer(answer) {
+            console.log(answer, currentQuestion.answer);
+            if (answer === currentQuestion.answer) {
+                console.log("correct");
+            } else {
+                console.log("incorrect");
+            }
+
+            start();
+        }
+
         function getNewQuestion() {
+            // Reset time
+            timeLeft = START_TIME;
+
+            // Clear the interval timer
+            clearInterval(intervalId);
+
+            // Set time interval to countd down every 1 second
+            intervalId = setInterval(countDown, 1000);
+
             // Get random question
-            let randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+            currentQuestion = questions[Math.floor(Math.random() * questions.length)];
 
             // Update display
             $("#seconds").text(START_TIME + " Seconds");
-            $("#question").text(randomQuestion.question);
+            $("#question").text(currentQuestion.question);
             for (let i = 0; i < 4; i++) {
                 // Get random option from the question's options
-                let option = randomQuestion.options[Math.floor(Math.random() * randomQuestion.options.length)];
+                let option = currentQuestion.options[Math.floor(Math.random() * currentQuestion.options.length)];
                 
                 // Remove the option from the question's options
-                randomQuestion.options.splice(randomQuestion.options.indexOf(option), 1);
+                currentQuestion.options.splice(currentQuestion.options.indexOf(option), 1);
 
                 // Set the option text to be the rand option
                 $("#option" + (i + 1)).text(option);
@@ -146,7 +176,7 @@
 
 
             // Remove the question for the array of questions
-            questions.splice(questions.indexOf(randomQuestion), 1);
+            questions.splice(questions.indexOf(currentQuestion), 1);
 
             console.log(questions);
         }
@@ -180,6 +210,23 @@
 
         // Start the game when the start button is clicked
         $("#start").on("click", start);
+
+        // Make a choice
+        $("#option1").on("click", function() {
+            checkAnswer(this.innerText);
+        });
+
+        $("#option2").on("click", function() {
+            checkAnswer(this.innerText);
+        });
+
+        $("#option3").on("click", function() {
+            checkAnswer(this.innerText);
+        });
+
+        $("#option4").on("click", function() {
+            checkAnswer(this.innerText);
+        });
 
         // Reset the game when the reset button is clicked
         $("#reset").on("click", reset);
